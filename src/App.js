cb15b4c56a7516c3f14263c8fd1c0184fbfaee1b
CoderRDX <rahuldev6258@gmail.com>
2024-09-19 00:46:59 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
-import './App.css';
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Footer from "./components/Footer";
+import DownloadApp from "./components/Sections/DownloadApp";
 
 function App() {
   return (
     <div className="App">
-     hello
+    
+     <CssBaseline />
+      <Outlet />
+      <DownloadApp />
+      <Footer />
     </div>
   );
 }
